1ca9562ccf9bd350d51b1a3e6409ad5811a94be2
PatrykWiewiorski <patryk.wiewiorski1@gmail.com>
2021-05-09 15:23:57 +0200

    rozbudowa1

--- Program.cs
+++ Program.cs
@@ -4,13 +4,32 @@ namespace biblioteka
 {
     class Program
     {
+        struct ksiazka{
+            public string tytul;
+            public string imie;
+            public string nazwisko;
+            public string datawydania;
+
+        }
         static void Main(string[] args)
         {
+            ksiazka ks1 =   new ksiazka();
+
             Console.WriteLine("Witaj w konsolowej aplikacji biblioteki!");
             Console.WriteLine("Wybierz 1 i wciśnij enter aby dodać książkę");
             Console.WriteLine("Wybierz 2 i wciśnij enter aby znaleźć książkę po tytule");
             Console.WriteLine("Wybierz 1 i wciśnij enter aby znaleźć książkę po imieniu i nazwisku autora");
+
+            int wybor = Convert.ToInt32(Console.ReadLine());
             
+            switch (wybor)
+            {
+                case1:
+                    Console.WriteLine("Podaj Imię autora");
+
+
+
+            }
         }
     }
 }
