6658cca82e4b3a24a114e31b10e108a8448c25b3
PatrykWiewiorski <patryk.wiewiorski1@gmail.com>
2021-05-09 15:12:55 +0200

    Początki

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace biblioteka
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Witaj w konsolowej aplikacji biblioteki!");
             Console.WriteLine("Wybierz 1 i wciśnij enter aby dodać książkę");
+            Console.WriteLine("Wybierz 2 i wciśnij enter aby znaleźć książkę po tytule");
+            Console.WriteLine("Wybierz 1 i wciśnij enter aby znaleźć książkę po imieniu i nazwisku autora");
+            
         }
     }
 }
