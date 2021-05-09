14b17ba2ca6f48d3acc1b970ed9e7183ce56928d
PatrykWiewiorski <patryk.wiewiorski1@gmail.com>
2021-05-09 15:33:10 +0200

    dodanie książki

--- Program.cs
+++ Program.cs
@@ -24,10 +24,31 @@ namespace biblioteka
             
             switch (wybor)
             {
-                case1:
-                    Console.WriteLine("Podaj Imię autora");
+                case 1:
+                    Console.WriteLine("Podaj tytuł książki");
+                    string tytul = Console.ReadLine();
+                    Console.WriteLine("Podaj Imię autora książki");
+                    string imie = Console.ReadLine();
+                    Console.WriteLine("Podaj Nazwisko autora książki");
+                    string nazwisko = Console.ReadLine();
+                    Console.WriteLine("Podaj rok wydania książki");
+                    string datawydania = Console.ReadLine();
 
+                        ks1.tytul= tytul;
+                        ks1.imie = imie;
+                        ks1.nazwisko = nazwisko;
+                        ks1.datawydania = datawydania;
+                        
+                    Console.WriteLine("Pomyślnie dodano książkę!");
 
+                    break;
+                case 2:
+                     break;
+                case 3:
+                     break;
+                default:
+                Console.WriteLine("Podaną złą wartość!");
+                break;
 
             }
         }
